ba4a07a42dc56efda29fc6a72d48ef2ef9c43272
Daniel Bitzer <danielbitzer@me.com>
2016-11-16 14:50:36 +1030

    housekeeping

--- addon-includes/abstract-addon.php
+++ addon-includes/abstract-addon.php
@@ -7,8 +7,8 @@
 if ( ! class_exists('AW_Abstract_Addon') ):
 
 
-	abstract class AW_Abstract_Addon
-	{
+	abstract class AW_Abstract_Addon {
+
 		/** @var string */
 		public $id;
 
@@ -60,8 +60,7 @@ if ( ! class_exists('AW_Abstract_Addon') ):
 		/**
 		 * Constructor for add-on, core plugin not be loaded at this point
 		 */
-		function __construct()
-		{
+		function __construct() {
 			add_action( 'admin_notices', array( $this, 'admin_notices' ) );
 			add_action( 'automatewoo_init_addons', array( $this, 'maybe_init' ) );
 		}
@@ -71,8 +70,7 @@ if ( ! class_exists('AW_Abstract_Addon') ):
 		 * @param string $end
 		 * @return string
 		 */
-		function url( $end = '' )
-		{
+		function url( $end = '' ) {
 			return untrailingslashit( plugin_dir_url( $this->plugin_basename ) ) . $end;
 		}
 
@@ -81,28 +79,24 @@ if ( ! class_exists('AW_Abstract_Addon') ):
 		 * @param string $end
 		 * @return string
 		 */
-		function path( $end = '' )
-		{
+		function path( $end = '' ) {
 			return untrailingslashit( $this->plugin_path ) . $end;
 		}
 
 
 		/**
+		 * Check the AutomateWoo class is loaded as the plugin could be activated but not passed env check
 		 * @return bool
 		 */
-		function is_automatewoo_active()
-		{
-			$active_plugins = (array) get_option( 'active_plugins', array() );
-
-			return in_array( 'automatewoo/automatewoo.php', $active_plugins ) || array_key_exists( 'automatewoo/automatewoo.php', $active_plugins );
+		function is_automatewoo_active() {
+			return class_exists( 'AutomateWoo' );
 		}
 
 
 		/**
 		 * @return bool
 		 */
-		function is_automatewoo_version_ok()
-		{
+		function is_automatewoo_version_ok() {
 			return version_compare( AW()->version, $this->required_automatewoo_version, '>=' );
 		}
 
@@ -110,8 +104,8 @@ if ( ! class_exists('AW_Abstract_Addon') ):
 		/**
 		 * @return bool
 		 */
-		function is_woocommerce_version_ok()
-		{
+		function is_woocommerce_version_ok() {
+
 			if ( ! function_exists( 'WC' ) ) return false;
 
 			if ( ! $this->required_woocommerce_version ) return true;
@@ -123,21 +117,18 @@ if ( ! class_exists('AW_Abstract_Addon') ):
 		/**
 		 * Adds an admin notice if required
 		 */
-		function admin_notices()
-		{
-			if ( ! $this->is_automatewoo_active() )
-			{
+		function admin_notices() {
+
+			if ( ! $this->is_automatewoo_active() ) {
 				$message = sprintf(__( '<strong>%s</strong> requires AutomateWoo to be installed and activated.', 'automatewoo' ), $this->name );
 				echo '<div class="notice notice-warning"><p>' . $message . '</p></div>' . "\n";
 			}
-			else if ( ! $this->is_automatewoo_version_ok() )
-			{
+			else if ( ! $this->is_automatewoo_version_ok() ) {
 				$message = sprintf(__( '<strong>%s</strong> requires AutomateWoo version %s or later. Please update to the latest version.', 'automatewoo' ), $this->name, $this->required_automatewoo_version );
 				echo '<div class="notice notice-warning"><p>' . $message . '</p></div>' . "\n";
 			}
 
-			if ( ! $this->is_woocommerce_version_ok() )
-			{
+			if ( ! $this->is_woocommerce_version_ok() ) {
 				$message = sprintf(__( '<strong>%s</strong> requires WooCommerce version %s or later.', 'automatewoo' ), $this->name, $this->required_woocommerce_version );
 				echo '<div class="notice notice-warning"><p>' . $message . '</p></div>' . "\n";
 			}
@@ -147,19 +138,17 @@ if ( ! class_exists('AW_Abstract_Addon') ):
 		/**
 		 * Check the version stored in the database and determine if an upgrade needs to occur
 		 */
-		function check_version()
-		{
+		function check_version() {
+
 			if (  $this->options()->version == $this->version )
 				return;
 
 			$this->install();
 
-			if ( $this->is_database_upgrade_available() )
-			{
+			if ( $this->is_database_upgrade_available() ) {
 				add_action( 'admin_notices', [ $this, 'data_upgrade_prompt' ] );
 			}
-			else
-			{
+			else {
 				$this->update_database_version();
 			}
 		}
@@ -168,8 +157,8 @@ if ( ! class_exists('AW_Abstract_Addon') ):
 		/**
 		 * @return bool
 		 */
-		function is_database_upgrade_available()
-		{
+		function is_database_upgrade_available() {
+
 			if ( $this->options()->version == $this->version )
 				return false;
 
@@ -180,14 +169,12 @@ if ( ! class_exists('AW_Abstract_Addon') ):
 		/**
 		 * Handle updates
 		 */
-		function do_database_update()
-		{
+		function do_database_update() {
+
 			@ini_set( 'memory_limit', apply_filters( 'admin_memory_limit', WP_MAX_MEMORY_LIMIT ) );
 
-			foreach ( $this->db_updates as $update )
-			{
-				if ( version_compare( $this->options()->version, $update, '<' ) )
-				{
+			foreach ( $this->db_updates as $update ) {
+				if ( version_compare( $this->options()->version, $update, '<' ) ) {
 					include( $this->path( "/includes/updates/$update.php" ) );
 				}
 			}
@@ -199,8 +186,7 @@ if ( ! class_exists('AW_Abstract_Addon') ):
 		/**
 		 * Update version to current
 		 */
-		function update_database_version()
-		{
+		function update_database_version() {
 			update_option( $this->options()->prefix . 'version', $this->version, true );
 		}
 
@@ -208,8 +194,7 @@ if ( ! class_exists('AW_Abstract_Addon') ):
 		/**
 		 * Renders prompt notice for user to update
 		 */
-		function data_upgrade_prompt()
-		{
+		function data_upgrade_prompt() {
 			AW()->admin->get_view( 'data-upgrade-prompt', [
 				'plugin_name' => $this->name,
 				'plugin_slug' => $this->plugin_slug
@@ -220,14 +205,15 @@ if ( ! class_exists('AW_Abstract_Addon') ):
 		/**
 		 *
 		 */
-		function maybe_init()
-		{
-			if ( ! $this->is_automatewoo_active() || ! $this->is_automatewoo_version_ok() || ! $this->is_woocommerce_version_ok() ) return;
+		function maybe_init() {
+
+			if ( ! $this->is_automatewoo_active() || ! $this->is_automatewoo_version_ok() || ! $this->is_woocommerce_version_ok() ) {
+				return;
+			}
 
 			AW()->addons()->register( $this );
 
-			if ( AW()->licenses->is_active( $this->id ) )
-			{
+			if ( AW()->licenses->is_active( $this->id ) ) {
 				$this->init();
 			}
 		}
@@ -236,8 +222,7 @@ if ( ! class_exists('AW_Abstract_Addon') ):
 		/**
 		 * Runs when the license for the add-on is activated
 		 */
-		function activate()
-		{
+		function activate() {
 			flush_rewrite_rules();
 		}
 
@@ -245,11 +230,9 @@ if ( ! class_exists('AW_Abstract_Addon') ):
 		/**
 		 * @return string
 		 */
-		function admin_start_url()
-		{
+		function admin_start_url() {
 			return '';
 		}
-
 
 	}
 
