fa4f272b417072c53fe90822406d91d5dfaa8653
Daniel Bitzer <danielbitzer@me.com>
2017-01-18 15:45:30 +1030

    code style

--- includes/actions/update-contact-field.php
+++ includes/actions/update-contact-field.php
@@ -1,29 +1,23 @@
 <?php
-/**
- * @class       AW_Action_AgileCRM_Update_Contact_Field
- * @package     AutomateWoo/Addons/AgileCRM
- * @since       1.0.0
- */
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
+/**
+ * @class AW_Action_AgileCRM_Update_Contact_Field
+ * @since 1.0
+ */
+class AW_Action_AgileCRM_Update_Contact_Field extends AW_Action_AgileCRM_Abstract {
 
-class AW_Action_AgileCRM_Update_Contact_Field extends AW_Action_AgileCRM_Abstract
-{
 	public $name = 'agilecrm_update_contact_field';
 
-	/**
-	 * Init
-	 */
-	public function init()
-	{
+
+	public function init() {
 		$this->title = __( 'Update Contact Custom Field', 'automatewoo-agilecrm' );
 		parent::init();
 	}
 
 
-	public function load_fields()
-	{
+	public function load_fields() {
 		$field_name = ( new AW_Field_Text_Input() )
 			->set_name('field_name')
 			->set_title( __( 'Custom Field Name', 'automatewoo-agilecrm' ) )
@@ -42,8 +36,8 @@ class AW_Action_AgileCRM_Update_Contact_Field extends AW_Action_AgileCRM_Abstrac
 	/**
 	 * @return void
 	 */
-	public function run()
-	{
+	public function run() {
+
 		$email = aw_clean_email( $this->get_option( 'email', true ) );
 		$field_name = aw_clean( $this->get_option( 'field_name' ) );
 		$field_value = aw_clean( $this->get_option( 'field_value', true ) );
@@ -53,8 +47,7 @@ class AW_Action_AgileCRM_Update_Contact_Field extends AW_Action_AgileCRM_Abstrac
 
 		$contact_id = AW_AgileCRM()->api()->get_contact_id_by_email( $email );
 
-		if ( $contact_id )
-		{
+		if ( $contact_id ) {
 			// add tags
 			$response = AW_AgileCRM()->api()->request( 'PUT', '/contacts/edit-properties', [
 				'id' => $contact_id,
